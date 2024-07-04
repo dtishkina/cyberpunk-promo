be27a1438a85e503dbf33cac1805fab00237c5fa
dtishkina <lfhmztshkn@gmail.com>
2024-07-04 11:52:57 +0300

    add about section

--- js/main.js
+++ js/main.js
@@ -3,4 +3,5 @@ import {loadSection} from "./loader.js";
 document.addEventListener('DOMContentLoaded', () => {
     loadSection('sections/header.html', 'header');
     loadSection('sections/hero.html', 'hero');
+    loadSection('sections/about.html', 'about');
 });
