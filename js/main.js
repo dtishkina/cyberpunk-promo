52614b2c21b9929cc08333a7b984c902eaad44b5
dtishkina <lfhmztshkn@gmail.com>
2024-07-10 17:10:05 +0300

    Add all files from master to gh-pages

---
+++ js/main.js
@@ -0,0 +1,11 @@
+import {loadSection} from "./loader.js";
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadSection('sections/header.html', 'header');
+    loadSection('sections/hero.html', 'hero');
+    loadSection('sections/about.html', 'about');
+    loadSection('sections/lottery.html', 'lottery');
+    loadSection('sections/hp.html', 'hp');
+    loadSection('sections/buy.html', 'buy');
+    loadSection('sections/footer.html', 'footer');
+});
